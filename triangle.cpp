338984d1f5db7b0d0c193bcdda53d2bdfe008429
Aman Naveen <aman.naveen@ramanujan.du.ac.in>
2025-03-17 10:23:06 +0530

    Create triangle.cpp

---
+++ triangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Triangle {
+public:
+    // Area with base & height
+    double area(double base, double height) {
+        return 0.5 * base * height;
+    }
+
+    // Area with 3 sides (Heron's formula)
+    double area(double a, double b, double c) {
+        double s = (a + b + c) / 2;
+        return sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+};
+
+int main() {
+    Triangle t;
+    cout << "Area (Base, Height): " << t.area(5, 10) << endl;
+    cout << "Area (Three Sides): " << t.area(3, 4, 5) << endl;
+    return 0;
+}
